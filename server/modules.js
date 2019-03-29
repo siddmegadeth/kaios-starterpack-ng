dda184eca397daef2a5d85cd35acfaedaeb0c652
Siddharth Chandra <siddmegadeth@gmail.com>
2019-03-29 13:51:31 +0530

    KaiOS Seed Final

---
+++ server/modules.js
@@ -0,0 +1,9 @@
+(function() {
+    express = module.exports = require("express");
+    http = module.exports = require("http");
+    log = console.log.bind(console);
+    app = express();
+    bodyParser = module.exports = require('body-parser');
+    puppeteer = module.exports = require('puppeteer');
+
+})()

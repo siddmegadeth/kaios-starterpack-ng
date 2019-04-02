1ea02f0f5d699fbf03dd752072b856c18c974ae1
Siddharth Chandra <siddharth_chandra@persistent.co.in>
2019-04-02 18:00:33 +0530

    Added Repeat

--- public/components/landing/landingCtrl.js
+++ public/components/landing/landingCtrl.js
@@ -1,14 +1,28 @@
 app.controller("landingCtrl", ['$scope', function($scope) {
 
-    naviBoard.setNavigation('navigateLanding');
+  $scope.cards = [];
+  naviBoard.setNavigation('navigateLanding');
+
+  $scope.generateCards = function(count) {
+    var i = 0;
+    while (i != count) {
+      var tuple = {};
+      tuple.title = "This Is A Card Header " + i;
+      tuple.footer = "This Is A Footer Card Text " + i;
+      tuple.header = "This Is A Card Header " + i;
+      $scope.cards.push(tuple);
+      i++;
+    }
+  }
 
 
+  $scope.$on("$destroy", function() {
 
-    $scope.$on("$destroy", function() {
+    log(" landingCtrl Navigation Destroyed");
+    naviBoard.destroyNavigation('navigateLanding');
 
-        log(" landingCtrl Navigation Destroyed");
-        naviBoard.destroyNavigation('navigateLanding');
+  });
 
-    });
+  $scope.generateCards(10);
 
 }]);
